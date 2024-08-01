a47341b35eab78bf6b260eb1ecf58d887e16a5be
Ilya <52174291+Ilia1099@users.noreply.github.com>
2024-08-01 21:47:59 +0300

    reworked structure to implement different versions of api -> moved router.py into appropriate package; changed basic url to implement different versions of api

--- drCandy/urls.py
+++ drCandy/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from api.router import router as rt
+from api.v1 import router as rv1
 from .views import MainPageView
 
 urlpatterns = [
     path('', MainPageView.as_view(), name='main'),
-    path('admin/', admin.site.urls),
-    path('api/', include(rt.urls)),
+    path('admin/', admin.site.urls, name='admin'),
+    path(f'api/v1/', include(rv1.urls), name='api_v1'),
 ]
 
 
