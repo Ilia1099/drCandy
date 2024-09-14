ff601826351a1fb2f5e764454cee012d6bb4c82b
Ilya <52174291+Ilia1099@users.noreply.github.com>
2024-09-14 17:14:13 +0300

    refactored endpoints' registration in router.py

--- api/v1/router.py
+++ api/v1/router.py
@@ -3,8 +3,8 @@ from gallery.views import BakeryViewSet, IngredientViewSet, BakeryTypeViewSet
 from shopping_cart.views import OrderViewSet, CartViewSet
 
 router = DefaultRouter()
-router.register(r'bakery', BakeryViewSet)
+router.register(r'bakeries', BakeryViewSet)
 router.register(r'ingredients', IngredientViewSet)
 router.register(r'bakery_types', BakeryTypeViewSet)
-router.register(r'shopping_cart', CartViewSet)
-router.register(r'order', OrderViewSet)
+router.register(r'order_items', CartViewSet)
+router.register(r'orders', OrderViewSet)
