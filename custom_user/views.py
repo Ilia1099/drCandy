302efb2cbdc2c396fd18434b3cc7382aa28302d0
Ilya <52174291+Ilia1099@users.noreply.github.com>
2024-10-12 11:30:14 +0300

    Created UsersViewSet

--- custom_user/views.py
+++ custom_user/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.generics import mixins
+from .serializers import CustomerSerializer, CustomerProfileSerializer
+from .models import CustomerProfile, User
 
-# Create your views here.
+
+class UsersViewSet(viewsets.ModelViewSet, mixins.CreateModelMixin, mixins.RetrieveModelMixin,
+                   mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+
+    lookup_field = 'id'
+    serializer_class = CustomerSerializer
+    queryset = User.objects.all()
+
