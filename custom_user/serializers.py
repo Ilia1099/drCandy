1a8f486bcf2294dd0fa5ddfb38a9f8c394170f54
Ilya <52174291+Ilia1099@users.noreply.github.com>
2024-10-12 11:31:56 +0300

    Updated CustomerSerializer, CustomerProfileSerializer

--- custom_user/serializers.py
+++ custom_user/serializers.py
@@ -3,16 +3,28 @@ from .models import User, CustomerProfile
 
 
 class CustomerSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
     class Meta:
         model = User
-        fields = ['id', 'mobile_number', 'email', 'is_active', 'date_created', 'date_updated']
-        read_only_fields = ['id', 'is_active', 'date_created', 'date_updated']
+        fields = ['mobile_number', 'password', 'email', 'first_name', 'last_name', 'username', 'is_customer']
+        read_only_fields = ['id', 'is_active', 'date_joined']
+        extra_kwargs = {
+            'password': {'write_only': True, 'required': True},
+            'email': {'required': True},
+            'first_name': {'required': True},
+            'last_name': {'required': True},
+            'username': {'required': True},
+            'is_customer': {'required': True}
+        }
 
 
 class CustomerProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomerProfile
-        fields = ['customer_id', 'first_name', 'last_name', 'profile_img', 'date_of_birth', 'date_created',
+        fields = ['customer_id', 'profile_img', 'date_of_birth', 'date_created',
                   'date_updated']
         read_only_fields = ['customer_id', 'date_created', 'date_updated', 'profile_img',]
 
