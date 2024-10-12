e0cc7389a26d0e6e6033f3e40325ab763168ba5a
Ilya <52174291+Ilia1099@users.noreply.github.com>
2024-10-12 11:38:26 +0300

    Added UsersViewSet endpoint to the router.py

--- api/v1/router.py
+++ api/v1/router.py
@@ -1,6 +1,7 @@
 from rest_framework.routers import DefaultRouter
 from gallery.views import BakeryViewSet, IngredientViewSet, BakeryTypeViewSet
 from shopping_cart.views import OrderViewSet, CartViewSet
+from custom_user.views import UsersViewSet
 
 router = DefaultRouter()
 router.register(r'bakeries', BakeryViewSet)
@@ -8,3 +9,5 @@ router.register(r'ingredients', IngredientViewSet)
 router.register(r'bakery_types', BakeryTypeViewSet)
 router.register(r'order_items', CartViewSet)
 router.register(r'orders', OrderViewSet)
+router.register('users', UsersViewSet)
+
