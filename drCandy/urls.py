012cdbd23372624ee210cd3dff4b1fbb3e10390d
Ilya <52174291+Ilia1099@users.noreply.github.com>
2024-10-28 20:45:37 +0200

    updated root url -> added authentication endpoint

--- drCandy/urls.py
+++ drCandy/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from api.v1.router import urlpatterns as api_urls
+from api_auth.views import SessionLogin
 from .views import MainPageView
 
 urlpatterns = [
     path('', MainPageView.as_view(), name='main'),
     path('admin/', admin.site.urls, name='admin'),
     path(f'api/v1/', include(api_urls), name='api_v1'),
+    path(f'auth/', SessionLogin.as_view(), name='auth'),
 ]
 
 
