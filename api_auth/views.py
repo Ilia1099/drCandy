efec077fc4dc9370ea7499e29d4d3fb982fe1609
Ilya <52174291+Ilia1099@users.noreply.github.com>
2024-10-28 20:20:41 +0200

    created endpoint for user authentication, basic functionality so far

---
+++ api_auth/views.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import authenticate, login, logout
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
+
+
+class SessionLogin(APIView):
+    def post(self, request):
+        username = request.data['username']
+        password = request.data['password']
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return Response({'detail': 'Login Successful'}, status=HTTP_200_OK)
+        return Response({'detail': 'Invalid Credentials'}, status=HTTP_400_BAD_REQUEST)
