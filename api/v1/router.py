0725aa10433c4a6292b7ef12bae5a33d2c17d541
Ilya <52174291+Ilia1099@users.noreply.github.com>
2024-11-23 13:37:50 +0200

    refactored url paths within router.py

--- api/v1/router.py
+++ api/v1/router.py
@@ -1,24 +1,28 @@
 from rest_framework_nested import routers
 from gallery.views import BakeryViewSet, IngredientViewSet, BakeryTypeViewSet
 from shopping_cart.views import OrderViewSet, CartViewSet
-from custom_user.views import UsersViewSet
+from custom_user.views import UsersViewSet, CustomerProfileViewSet
 from django.urls import path, include
 
 base_router = routers.DefaultRouter()
-base_router.register('users', UsersViewSet, basename='users')
-
+base_router.register(r'account', UsersViewSet, basename='account')
 base_router.register(r'bakeries', BakeryViewSet)
 base_router.register(r'ingredients', IngredientViewSet)
 base_router.register(r'bakery_types', BakeryTypeViewSet)
 
-users_router = routers.NestedSimpleRouter(base_router, r'users', lookup='user')
-users_router.register(r'orders', OrderViewSet, basename='orders')
+account_router = routers.NestedSimpleRouter(base_router, r'account', lookup='user')
+account_router.register('orders', OrderViewSet, basename='orders')
+account_router.register('profile', CustomerProfileViewSet, basename='profile')
 
-orders_router = routers.NestedSimpleRouter(users_router, r'orders', lookup='order')
+orders_router = routers.NestedSimpleRouter(account_router, r'orders', lookup='order')
 orders_router.register(r'items', CartViewSet, basename='items')
+
 
 urlpatterns = [
     path('', include(base_router.urls)),
-    path('', include(users_router.urls)),
+    path('', include(account_router.urls)),
     path('', include(orders_router.urls)),
 ]
+
+# Todo
+# rework userviewst - change of password should not be mandatory
