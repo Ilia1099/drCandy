29b66d564e81df88a1770d1ce78137e57d6018f7
Ilya <52174291+Ilia1099@users.noreply.github.com>
2024-11-23 13:35:14 +0200

    updated UsersViewSet to adjust permissions depending on action; Implemented CustomerProfileViewSet;

--- custom_user/views.py
+++ custom_user/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, AllowAny
 
+from .permissions.custom_user_permissions import IsAminOrOwner, IsOwner
 from .serializers import CustomerSerializer, CustomerProfileSerializer
 from .models import CustomerProfile, User
 
@@ -13,7 +14,24 @@ class UsersViewSet(viewsets.ModelViewSet):
     serializer_class = CustomerSerializer
     queryset = User.objects.all()
 
+    def get_permissions(self):
+        if self.action == "list":
+            permission_classes = [IsAdminUser]
+        elif self.action == "create":
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAminOrOwner]
+        return [permission() for permission in permission_classes]
 
-class CustomerProfileViewSet(viewsets.ModelViewSet):
+
+class CustomerProfileViewSet(
+    viewsets.ModelViewSet
+):
     """Viewset which implements CRUD operations for users' profile endpoint - to be implemented"""
-    ...
+    permission_classes = [IsOwner]
+    serializer_class = CustomerProfileSerializer
+    queryset = CustomerProfile.objects.all()
+    lookup_field = 'customer_id_id'
+
+    def get_queryset(self):
+        return self.queryset.filter(customer_id_id=self.request.user.id)
