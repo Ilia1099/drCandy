3aa1467d702a1c52922eea9882821f0bd6a29a29
Ilya <52174291+Ilia1099@users.noreply.github.com>
2024-11-23 13:33:36 +0200

    created custom permissions

--- custom_user/permissions/custom_user_permissions.py
+++ custom_user/permissions/custom_user_permissions.py
@@ -1,6 +1,20 @@
 from rest_framework import permissions
 
 
+class IsAminOrOwner(permissions.BasePermission):
+    """custom permission to allow only owners of an object or admins to edit it """
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        if not request.user.is_authenticated:
+            return False
+        is_admin_or_owner = request.user.is_superuser or obj.id == request.user.id
+        return is_admin_or_owner
+
+
 class IsOwner(permissions.BasePermission):
-    """custom permission to only allow owners of an object to edit it -> to be reworked"""
-    ...
+    """permission which checks if the user is an owner of the object"""
+    def has_object_permission(self, request, view, obj):
+        if not request.user.is_authenticated:
+            return False
+        return obj.id == request.user.id
