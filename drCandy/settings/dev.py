3a353ac6ccd8e59e7c0961dc76c98a577f5f186c
Ilya <52174291+Ilia1099@users.noreply.github.com>
2025-01-31 18:46:33 +0200

    refactored UsersViewSet change if-else statements for match-case

--- drCandy/settings/dev.py
+++ drCandy/settings/dev.py
@@ -46,12 +46,12 @@ CACHES = {
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'drCandy_dev_base.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'drCandy_dev_base.sqlite3',
+#     }
+# }
 
 
 
